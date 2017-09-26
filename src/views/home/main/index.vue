73de31ef8b562ea3e653681984da6b1d4454cc31
jspwoody <jspwoody@qq.com>
2017-09-26 19:26:50 +0800

    --

--- src/views/home/main/index.vue
+++ src/views/home/main/index.vue
@@ -42,17 +42,17 @@
 
     const baseList = [{
         url: 'javascript:',
-        img: 'https://static.vux.li/demo/1.jpg',
+        img: '../../static/img/home/swiper/1.jpg',
         title: '送你一朵fua'
     }, {
         url: 'javascript:',
-        img: 'https://static.vux.li/demo/2.jpg',
+        img: '../../static/img/home/swiper/2.jpg',
         title: '送你一辆车'
     }, {
         url: 'javascript:',
         img: 'https://static.vux.li/demo/5.jpg',
         title: '送你一次旅行',
-        fallbackImg: 'https://static.vux.li/demo/3.jpg'
+        fallbackImg: '../../static/img/home/swiper/3.jpg'
     }];
 
 
@@ -117,7 +117,7 @@
                         name: service.name,
                         url: service.url,
                         pageParam: {
-                            name: 'test'
+                            name: 'test12'
                         }
                     });
                 } else {
